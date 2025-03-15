a858096e93dd9c6a09005b26b7864c8c7d778fa3
Max Verchynskyi <disablaksteam@gmail.com>
2025-03-15 13:35:41 +0200

    movement and collisions

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,2 +1,15 @@
 class_name Player
 extends CharacterBody2D
+
+
+@export var speed: float = 10
+
+
+func _update_velocity_by_inputs(delta):
+	var input_dir = Input.get_vector("left", "right", "up", "down")
+	velocity = input_dir * speed * delta
+
+
+func _physics_process(delta: float) -> void:
+	_update_velocity_by_inputs(delta)
+	move_and_slide()
